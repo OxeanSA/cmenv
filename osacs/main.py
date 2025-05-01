7e61590a96211c1e6faed09c5161de8a99718637
OxeanSA <oxeansa@gmail.com>
2025-05-01 09:30:21 +0000

    Added tornado to amin

--- osacs/main.py
+++ osacs/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 app = Flask(__name__)
 
@@ -8,4 +11,6 @@ def index():
     return render_template('index.html', the_title='Oxean-SA Home')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server = HTTPServer(WSGIContainer(app))
+    server.listen(port=5000)
+    IOLoop.instance().start()
